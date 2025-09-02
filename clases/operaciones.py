655f046431d609c12ce5d31103d6342e9bbb4e6f
Ana Paula Alvarez Cerda <193064-0@iberoleon.edu.mx>
2025-09-02 12:18:00 -0600

    mis cambios

--- clases/operaciones.py
+++ clases/operaciones.py
@@ -22,7 +22,15 @@ class Operaciones:
     
     def sumar(self):
         self.resultado = "La suma de " + str(self.num1) + " + " + str(self.num2) + " es igula a " + str(self.num1 + self.num2)
-    
+    def restar(self):
+        self.resultado = "La sresta " + str(self.num1) + " - " + str(self.num2) + " es igula a " + str(self.num1 - self.num2)
+    def multiplicar(self):
+        self.resultado = "La multiplcacion de " + str(self.num1) + " * " + str(self.num2) + " es igula a " + str(self.num1 * self.num2)
+    def dividir(self):
+        self.resultado = "La division de " + str(self.num1) + " / " + str(self.num2) + " es igula a " + str(self.num1 / self.num2)
+    def modulo(self):
+        self.resultado = "El modulo de " + str(self.num1) + " % " + str(self.num2) + " es igula a " + str(self.num1 % self.num2)
+        
     def mostrarResultado(self):
         print(self.resultado)
         
